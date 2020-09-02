d75fd2cdd08997f3a6d2b05cbb13ae4f7d1c1c26
afoolsbag <afoolsbag@gmail.com>
2020-09-02 16:15:39 +0800

    chore: 修改以允许通过 run.cmd 脚本传递参数

--- rrpyinstaller/run.cmd
+++ rrpyinstaller/run.cmd
@@ -15,7 +15,7 @@ CD "%script_directory%" ^
         && CALL :pause_if_double_click ^
         && EXIT /B 2
 
-pipenv run python "%source_directory_name%" ^
+pipenv run python "%source_directory_name%" %* ^
         || ECHO Run python %source_directory_name% failed. ^
         && CALL :pause_if_double_click ^
         && EXIT /B 3
